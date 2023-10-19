af393267dab1df541671def2aa872c92ef2d9b78
Mike Sowers <msowers72@yahoo.com>
2023-10-19 11:13:05 -0400

    init function is called when the page loads

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,3 +12,18 @@ var isWin = false;
 var timer;
 var timerCount;
 
+// Array used tocreate blanks and letters on screen
+var lettersInChosenWord = [];
+var blanksLetters = [];
+
+// Array of words the user will guess
+var words = ["variable", "array", "modulus", "object", "function", "string", "boolean"];
+
+// The init function is calle when the page loads
+function init() {
+    // TODO: getWins() function needs to be set-up
+    // TODO: getLosses() function needs to be set-up
+}
+
+// Calls init() so that it firs when the page opens
+init()
