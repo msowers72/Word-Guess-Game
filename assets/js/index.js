b9075fa24c5e1ae0d1032645bd0b6a91d4887eb6
Mike Sowers <msowers72@yahoo.com>
2023-10-19 11:08:52 -0400

    declared global variables and declared some global variables

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+var wordBlank = document.querySelector(".word-blanks");
+var win = document.querySelector(".win");
+var lose = document.querySelector(".lose");
+var timerElement = document.querySelector(".timer-count");
+var startButton = document.querySelector(".start-button")
+
+var chosenWord = "";
+var numBlanks = 0;
+var winCounter = 0;
+var loseCounter = 0;
+var isWin = false;
+var timer;
+var timerCount;
+
